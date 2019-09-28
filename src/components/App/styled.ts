d6a0ba2c21781384c46fc4390d382af1c50ccb9c
Tomasz Olędzki <luksfer@poczta.onet.pl>
2019-09-28 18:11:59 +0200

    Improve App background styling

--- src/components/App/styled.ts
+++ src/components/App/styled.ts
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 import background from '../../assets/background.jpg';
 
 export const StyledApp = styled.div`
-  background: url(${background});
+  background-image: url(${background});
+  background-position: center center;
   background-size: cover;
   height: 100%;
   width: 100%;
