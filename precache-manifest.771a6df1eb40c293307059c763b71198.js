4c435c6bbfdd17e8775545748e8a5ee91da8774a
Tomasz Olędzki <luksfer@poczta.onet.pl>
2019-09-30 00:23:23 +0200

    Updates

---
+++ precache-manifest.771a6df1eb40c293307059c763b71198.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f0a822523658c13dbae7036141458fd",
+    "url": "/github-issues-app/index.html"
+  },
+  {
+    "revision": "aa756b6b4b7edbe55525",
+    "url": "/github-issues-app/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "3e9d9c5874bf6f65438e",
+    "url": "/github-issues-app/static/css/main.80b332df.chunk.css"
+  },
+  {
+    "revision": "aa756b6b4b7edbe55525",
+    "url": "/github-issues-app/static/js/2.d8308445.chunk.js"
+  },
+  {
+    "revision": "3e9d9c5874bf6f65438e",
+    "url": "/github-issues-app/static/js/main.74725339.chunk.js"
+  },
+  {
+    "revision": "18e4be57e65bcf520189",
+    "url": "/github-issues-app/static/js/runtime-main.49d3f227.js"
+  },
+  {
+    "revision": "df3df160ec5806a4d00e39781bb01679",
+    "url": "/github-issues-app/static/media/background.df3df160.jpg"
+  },
+  {
+    "revision": "ded63a50ac58b6139d46464274c9bd89",
+    "url": "/github-issues-app/static/media/icon-closed-issue.ded63a50.svg"
+  },
+  {
+    "revision": "89b6c978a7a6212b74015da3221e0e86",
+    "url": "/github-issues-app/static/media/icon-github.89b6c978.svg"
+  },
+  {
+    "revision": "e0aa5534b8535a0bbcbeba56c393cf17",
+    "url": "/github-issues-app/static/media/icon-open-issue.e0aa5534.svg"
+  },
+  {
+    "revision": "276d25cebe2fd036bf09263bff682432",
+    "url": "/github-issues-app/static/media/icon-star.276d25ce.svg"
+  }
+]);
